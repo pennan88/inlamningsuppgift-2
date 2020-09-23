e42dfbb53606258806050cb686447fe958d18fed
Alexander Fagrelius <erikalexander98@hotmail.com>
2020-09-23 16:01:58 +0200

    Add project files.

---
+++ CarInfoAndBank.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.Encodings.Web;
+using Uppgift2;
+
+namespace Uppgift2
+{
+
+    //-----------------------------------------------------------------------------------------------------
+    // Bil klass.
+    //-----------------------------------------------------------------------------------------------------
+
+    public class Carinfo
+    {
+        public string Color = "";
+        public string Type = "";
+        public string Engine = "";
+        public string Model = "";
+        public int Cost;
+
+        //-----------------------------------------------------------------------------------------------------
+        // En constructor så man kan definera exempelvis vilken sorts bil.
+        //-----------------------------------------------------------------------------------------------------
+        public Carinfo(string _Type, string _Model, string _Engine, string _Color, int _Cost)
+        {
+            Engine = _Engine;
+            Type = _Type;
+            Model = _Model;
+            Color = _Color;
+            Cost = _Cost;
+        }
+
+        //-----------------------------------------------------------------------------------------------------
+        // En metod för att skapa bil där jag kallar på constuctorn.
+        //-----------------------------------------------------------------------------------------------------
+
+        public void CarInfo()
+        {
+            Console.WriteLine(Type + "_" + Model + " with a " + Engine + " engine, it comes with the color " + Color + " at the price of " + Cost + "$\n");
+        }
+
+        //-----------------------------------------------------------------------------------------------------
+        // Metod när ett köp körs
+        //-----------------------------------------------------------------------------------------------------
+
+        public void CarAuction()
+        {
+
+            if (BankAccount.Card_Balance.CardBalance < Cost)
+            {
+                Console.WriteLine("You cant afford that");
+                Console.ReadLine();
+
+            }
+            else
+            {
+                Console.WriteLine("You bought the " + Type + " " + Model + " for " + Cost + "$\n");
+
+                Console.WriteLine(Cost + "$ was charged from your account");
+                BankAccount.Card_Balance.CardBalance -= Cost;
+
+                Console.ReadLine();
+            }
+        }
+
+
+    }
+
+    //-----------------------------------------------------------------------------------------------------
+    // Klass för Bankkontot
+    //-----------------------------------------------------------------------------------------------------
+
+    public class BankAccount
+    {
+        public double Balance;
+        public double CardBalance;
+        public static BankAccount Card_Balance = new BankAccount();
+        public static BankAccount account01 = new BankAccount();
+
+        //-----------------------------------------------------------------------------------------------------
+        // Meoter för att kolla Pengarna man har på banken och sen på "Kortet" 
+        //-----------------------------------------------------------------------------------------------------
+
+        public void _Card_Balance()
+        {
+            Console.WriteLine("Balance card: " + Card_Balance.CardBalance + "$");
+        }
+        public void Bank_Balance()
+        {
+            Console.WriteLine("Balance bank: " + account01.Balance + "$");
+        }
+
+        //-----------------------------------------------------------------------------------------------------
+        // Här kan man lägga in pengar på banken från sitt kort
+        //-----------------------------------------------------------------------------------------------------
+
+        public void Bank_Deposit()
+        {
+            Console.WriteLine($"You deposited {Card_Balance.CardBalance}$ to your bank account.");
+            account01.Balance += Card_Balance.CardBalance;
+            Card_Balance.CardBalance -= Card_Balance.CardBalance;
+        }
+
+        //-----------------------------------------------------------------------------------------------------
+        // Här kan man ta ut pengar från banken till sit kort -- kort
+        //-----------------------------------------------------------------------------------------------------
+
+        public void Bank_Withdraw(int _num1)
+        {
+            if (account01.Balance < _num1)
+            {
+                Console.WriteLine("You dont have enough money to withdraw.");
+            }
+            else
+            {
+                int num1 = _num1;
+                Console.WriteLine("You withdrew " + _num1 + "$ from your bank account.");
+                account01.Balance -= num1;
+                Card_Balance.CardBalance += num1;
+            }
+        }
+    }
+
+    //-----------------------------------------------------------------------------------------------------
+    // Här en är en klass Job som även ärver från BankAccount
+    //-----------------------------------------------------------------------------------------------------
+
+    public class Job : BankAccount
+    {
+        public static Job jobtest = new Job();
+        public static double JobPay;
+        public Random Income = new Random();
+
+        public double Jobtest()
+        {
+            JobPay = Income.Next(500, 2500);
+            return JobPay;
+        }
+
+    }
+
+    //-----------------------------------------------------------------------------------------------------
+    // Klass som heter "Energy" ärver från job och här har man metoder så som "Work" och "Sleep"
+    //-----------------------------------------------------------------------------------------------------
+
+    public class Energy : Job
+    {
+        public static int Workenergy = 2;
+
+
+        public void Work()
+        {
+            if (Workenergy <= 0)
+            {
+                Console.WriteLine("You are too tired");
+                Console.ReadLine();
+
+            }
+
+            else
+            {
+                Console.WriteLine("You went to work....\n");
+                Console.WriteLine("You earned " + jobtest.Jobtest() + "$");
+                account01.Balance += JobPay;
+                Workenergy--;
+                Console.ReadLine();
+            }
+
+        }
+        public void Sleep()
+        {
+            if (Workenergy == 2)
+            {
+                Console.Clear();
+                Console.WriteLine("Can't sleep now, you are not tired.");
+                Console.ReadLine();
+            }
+            else if (Workenergy == 1)
+            {
+                Console.Clear();
+                Console.WriteLine("You went home and took a quick nap.");
+                Console.WriteLine("+ 1 energy");
+                Workenergy++;
+                Console.ReadLine();
+            }
+
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("You called it the day and went to sleep");
+                Console.WriteLine("+ 2 energy");
+                Workenergy += 2;
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace Uppgift2
+{
+    public class Program
+    {
+        public static Energy Daily = new Energy();
+        public static BankAccount BankOptions = new BankAccount();
+        public static string date = DateTime.Now.ToString("yyyy-MMMM-dd" + " HH:mm:ss tt\n");
+
+        static void Main(string[] args)
+        {
+
+
+            bool showMenu = true;
+            while (showMenu)
+            {
+
+                showMenu = MainMenu();
+            }
+
+        }
+        public static bool MainMenu()
+        {
+
+
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Carinfo car01 = new Carinfo("Audi", "R8", "V10", "Black", 570000);
+            Carinfo car02 = new Carinfo("BMW", "I6", "V8", "Red", 87900);
+            Carinfo car03 = new Carinfo("Honda", "Civic", "V8", "Black", 13400);
+
+
+
+            Console.Write(date);
+            Console.WriteLine("Daily Energy: " + Energy.Workenergy + "\n");
+            Console.WriteLine("1. Go to the car dealer. ");
+            Console.WriteLine("2. Go to the bank. ");
+            Console.WriteLine("3. Go to work. ");
+            Console.WriteLine("4. Go to sleep. ");
+            Console.WriteLine("5. Remove yourself from existence. ");
+
+
+            string MenuChange = Console.ReadLine();
+
+
+            switch (MenuChange)
+            {
+                case "1":
+                    Console.Clear();
+                    Console.WriteLine("Hello! Welcome to your local car dealer.\n");
+                    Console.WriteLine("These are the cars we have in store. \n");
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+
+                    car01.CarInfo();
+                    car02.CarInfo();
+                    car03.CarInfo();
+
+                    Console.ResetColor();
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+
+                    Console.WriteLine("Are you interested in any of these cars? \n");
+                    Console.WriteLine("Yes. ");
+                    Console.WriteLine("No. ");
+                    string CarList = Console.ReadLine();
+                    Console.Clear();
+
+                    switch (CarList)
+                    {
+
+                        case "y":
+                            Console.WriteLine("Which of the cars would you like to buy? ");
+                            Console.WriteLine("1. The Audi R8. 570000$");
+                            Console.WriteLine("2. The BMW I6. 87900$ ");
+                            Console.WriteLine("3. The Honda Civic. 13400$ ");
+                            string BuyCar = Console.ReadLine();
+                            Console.Clear();
+
+                            switch (BuyCar)
+                            {
+                                case "1":
+                                    car01.CarAuction();
+                                    return true;
+
+                                case "2":
+                                    car02.CarAuction();
+                                    return true;
+
+                                case "3":
+
+                                    car03.CarAuction();
+
+                                    return true;
+
+                            }
+                            break;
+
+                        case "n":
+                            Console.WriteLine("Okey, have a nice day! ");
+                            Console.ReadLine();
+                            break;
+
+                    }
+
+                    return true;
+
+                case "2":
+                    Console.Clear();
+                    Console.WriteLine("Welcome to the bank\n");
+                    Console.WriteLine("1. Checkings: ");
+                    Console.WriteLine("2. Deposit money to the bank: ");
+                    Console.WriteLine("3. Withdraw money from the bank: ");
+                    Console.WriteLine("4. Go back home.");
+                    string Bank = Console.ReadLine();
+
+                    switch (Bank)
+                    {
+                        case "1":
+
+                            Console.Clear();
+                            BankAccount.Card_Balance._Card_Balance();
+                            BankAccount.account01.Bank_Balance();
+
+                            Console.ReadLine();
+                            break;
+
+                        case "2":
+                            BankOptions.Bank_Deposit();
+                            Console.ReadLine();
+
+                            break;
+
+                        case "3":
+                            Console.Clear();
+                            Console.WriteLine("How much would you like to withdaw? ");
+                            Console.WriteLine("1. 1000$");
+                            Console.WriteLine("2. 3000$");
+                            Console.WriteLine("3. 5000$");
+                            Console.WriteLine("4. 10000$");
+                            Console.WriteLine("5. Everything");
+
+                            switch (Console.ReadLine())
+                            {
+                                case "1":
+                                    Console.Clear();
+                                    BankOptions.Bank_Withdraw(1000);
+                                    Console.ReadLine();
+                                    break;
+
+                                case "2":
+                                    Console.Clear();
+                                    BankOptions.Bank_Withdraw(3000);
+                                    Console.ReadLine();
+                                    break;
+
+                                case "3":
+                                    Console.Clear();
+                                    BankOptions.Bank_Withdraw(5000);
+                                    Console.ReadLine();
+                                    break;
+
+                                case "4":
+                                    Console.Clear();
+                                    BankOptions.Bank_Withdraw(10000);
+                                    Console.ReadLine();
+                                    break;
+                                case "5":
+                                    Console.Clear();
+                                    Console.WriteLine("You transfered " + BankAccount.account01.Balance + "$ \n");
+                                    BankAccount.Card_Balance.CardBalance += BankAccount.account01.Balance;
+                                    BankAccount.account01.Balance -= BankAccount.account01.Balance;
+                                    Console.ReadLine();
+                                    break;
+
+                            }
+
+                            break;
+
+                        case "4":
+                            return true;
+
+                        default:
+                            Console.WriteLine("Unknow input");
+                            Console.ReadLine();
+                            return true;
+                    }
+                    return true;
+
+                case "3":
+
+                    {
+                        Daily.Work();
+                    }
+
+                    return true;
+
+                case "4":
+                    {
+                        Daily.Sleep();
+                    }
+
+                    return true;
+
+                case "5":
+                    return false;
+
+                default:
+                    Console.WriteLine("Unknow input");
+                    Console.ReadLine();
+                    return true;
+            }
+        }
+    }
+}
